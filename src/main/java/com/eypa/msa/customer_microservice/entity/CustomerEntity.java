248141883bfa5a60a7065dee13d3bde2d2ac0115
epuga <yoel338@hotmail.com>
2025-07-12 22:15:11 -0500

    update getter and setters

--- src/main/java/com/eypa/msa/customer_microservice/entity/CustomerEntity.java
+++ src/main/java/com/eypa/msa/customer_microservice/entity/CustomerEntity.java
@@ -29,5 +29,41 @@ public class CustomerEntity {
 	private String email;
 	private String phone_number;
 	private LocalDateTime created_at = LocalDateTime.now();
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getFirst_name() {
+		return first_name;
+	}
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+	public String getLast_name() {
+		return last_name;
+	}
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhone_number() {
+		return phone_number;
+	}
+	public void setPhone_number(String phone_number) {
+		this.phone_number = phone_number;
+	}
+	public LocalDateTime getCreated_at() {
+		return created_at;
+	}
+	public void setCreated_at(LocalDateTime created_at) {
+		this.created_at = created_at;
+	}
 
 }
